8c7ca68f31a90620b9cfec9e659ee22ec256126d
Artaey <95622783+Artaey@users.noreply.github.com>
2023-05-25 11:29:26 +0200

    update

--- lifa.js
+++ lifa.js
@@ -1,19 +1,39 @@
 let button = document.getElementById("button");
-console.log(button);
+let topswoosh = document.getElementById("totop");
 
-setTimeout(first(), "1000");
+setTimeout(first, "2000");
+topswoosh.style.opacity = "0";
+topswoosh.style.pointerEvents = "none"
+
+function show(){
+    let y = window.scrollY;
+    if(y >= 300){
+        topswoosh.style.opacity = "1";
+        topswoosh.style.pointerEvents = "all"
+    }else{
+        topswoosh.style.opacity = "0";
+        topswoosh.style.pointerEvents = "none"
+    }
+}
+
+function totop(){
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
 
 function first(){
     button.style.transform = "rotate(7deg)";
-    setTimeout(second(), "100");
+    setTimeout(second, "100");
 }
 
 function second(){
     button.style.transform = "rotate(-7deg)";
-    setTimeout(reset(), "100");
+    setTimeout(reset, "100");
 }
 
 function reset(){
     button.style.transform = "rotate(0deg)";
-    setTimeout(first(), "1000");
+    setTimeout(first, "2000");
 }
+
+topswoosh.addEventListener("click", totop);
+window.addEventListener("scroll", show);
